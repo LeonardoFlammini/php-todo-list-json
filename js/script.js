fe7e8a28484cfc7a45b04c6b96a01521dbac4618
LeonardoFlammini <leonardo98flammini@gmail.com>
2023-10-31 12:00:16 +0100

    [add] toggle 'done', controllo task valido, finito

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ createApp({
       apiUrl: "./server.php",
       list : [],
       newTaskModel: '',
+      message: ''
 
     }
   },
@@ -18,14 +19,22 @@ createApp({
         })
     },
     addTask(){
-      const data = new FormData();
-      data.append('newTask' , this.newTaskModel);
-
-      axios.post(this.apiUrl, data)
-        .then(result =>{
-          this.list = result.data;
-        });
-      this.newTaskModel = '';
+      if(this.newTaskModel != ''){
+        this.message = '';
+        const data = new FormData();
+        data.append('newTask' , this.newTaskModel);
+  
+        axios.post(this.apiUrl, data)
+          .then(result =>{
+            this.list = result.data;
+          });
+        this.newTaskModel = '';
+      }else{
+        this.message = "Inserisci un task valido";
+        setTimeout(() => {
+          this.message = '';
+        }, 3000);
+      }
     },
     removeTask(index){
       const data = new FormData();
--- server.php
+++ server.php
@@ -3,15 +3,14 @@
 $toDoString = file_get_contents('data.json');
 $toDoArray = json_decode($toDoString, true);
 
-class task {
-  public $text;
-  public $done;
-}
+
+
 
 if(isset($_POST['newTask'] ) && !empty($_POST['newTask'])){
-  $newTask = new task();
-  $newTask->text = $_POST['newTask'];
-  $newTask->done = false;
+  $newTask = [
+    "text" => $_POST['newTask'],
+    "done" => false
+  ];
   $toDoArray[] = $newTask;
   file_put_contents ('data.json', json_encode($toDoArray));
 }
@@ -24,9 +23,7 @@ if(isset($_POST['indexToDelete'] )){
 
 if(isset($_POST['indexToToggle'] )){
   $index = $_POST['indexToToggle'];
-  $taskSelected = $toDoArray[$index];
-  $taskSelected->done = !$taskSelected->done;
-  var_dump($toDoArray);
+  $toDoArray[$index]["done"] = !$toDoArray[$index]["done"] ;
   file_put_contents ('data.json', json_encode($toDoArray));
 }
 
